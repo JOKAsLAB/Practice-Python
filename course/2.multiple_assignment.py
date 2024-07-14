80885dde454f9c651bc9efef92a6faccbd3a8817
JocaMartins33 <jokaslab233@gmail.com>
2024-07-14 22:02:55 +0100

    Practice-Python

---
+++ course/2.multiple_assignment.py
@@ -0,0 +1,16 @@
+# multiple assignment
+
+name , age, tall = "Joca", 20, True
+
+print(name)
+print(age)
+print(tall)
+
+#----------------------------------#
+
+Joca = Orlando = Pinho = David = "LECI"
+
+print(Joca)
+print(Orlando)
+print(Pinho)
+print(David)
