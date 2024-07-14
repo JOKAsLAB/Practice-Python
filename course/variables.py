a2cf4c6c725086a1f13972fe95d9b00b0bc3704c
JocaMartins33 <jokaslab233@gmail.com>
2024-07-14 21:31:26 +0100

    Practice-Python

---
+++ course/variables.py
@@ -0,0 +1,44 @@
+# variables
+
+#----------------str (strings)----------------#
+
+first_name = "Joca"
+last_name = "Martins"
+full_name = first_name + " " + last_name
+
+print(first_name)
+print(type(first_name))
+
+print("Ola")
+print("Ola " + first_name)
+
+print(last_name)
+print(full_name)
+print("Ola " + full_name)
+
+
+#----------------int (integer)----------------#
+
+age = 20
+age_next_year = age + 1
+
+print(age)
+print(type(age))
+print(age_next_year)
+
+print("Your age is " + str(age))
+
+
+#--------------------float--------------------#
+
+height = 250.5
+
+print("Your height is " + str(height) + "cm")
+print(type(height))
+
+
+#---------------------bool--------------------#
+
+human = True
+print(type(human))
+print("Are you a human: " + str(human))
