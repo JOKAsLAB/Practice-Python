232a525a028f9e01bf358e862540760632cc7b7a
JocaMartins33 <jokaslab233@gmail.com>
2024-07-15 23:51:25 +0100

    Practice-Python

---
+++ course/11.for_loops.py
@@ -0,0 +1,27 @@
+# for loops
+import time
+
+for i in range(10):
+    print(i+1)
+    
+print()
+
+for i in range(50, 100+1):
+    print(i)
+    
+print()    
+    
+for i in range(10, 20+1, 2):
+    print(i)
+
+print()
+
+for i in "Joca Martins":
+    print(i)
+    
+print()
+
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
