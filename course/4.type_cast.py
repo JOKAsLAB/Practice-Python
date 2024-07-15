e20f258df9da735f4287b12531e0e1e159a8c4bc
JocaMartins33 <jokaslab233@gmail.com>
2024-07-15 14:20:54 +0100

    Practice-Python

---
+++ course/4.type_cast.py
@@ -0,0 +1,20 @@
+# type cast
+
+x = 1       # int
+y = 2.0     # float
+z = "3"     # str
+
+print(x)
+print(y)
+print(z)
+
+print()
+
+print(float(x))
+print(int(y))
+print(int(z) * 9)
+
+print()
+
+number = 3
+print("Your number is " + str(number))
