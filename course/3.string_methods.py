84fc159a08cbdf361bf580fa858c6e7484d6f00a
JocaMartins33 <jokaslab233@gmail.com>
2024-07-15 13:17:18 +0100

    Practice-Python

---
+++ course/3.string_methods.py
@@ -0,0 +1,16 @@
+# string methods
+
+name = "Joca"
+print(len(name))
+print(name.find("J"))
+print(name.find("a"))
+
+name2 = "joaquim martins"
+print(name2.capitalize())
+print(name2.upper())
+print(name2.lower())
+print(name2.isdigit())
+print(name2.isalpha())
+print(name2.count("a"))
+print(name2.replace("a","4"))
+print(name2*3)
