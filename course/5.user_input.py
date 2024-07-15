cb8caacd1ccd1ef65adc832c92342fdcea2f61d5
JocaMartins33 <jokaslab233@gmail.com>
2024-07-15 14:27:12 +0100

    Practice-Python

---
+++ course/5.user_input.py
@@ -0,0 +1,11 @@
+# user input
+
+name = input("What is your name?: ")
+print("Hello " + name)
+
+age = int(input("How old are you?: "))
+age = age + 1
+print("Your age next year is " + str(age))
+
+height = float(input("How tall are you?: "))
+print("You are " + str(height) + " cm tall.")
