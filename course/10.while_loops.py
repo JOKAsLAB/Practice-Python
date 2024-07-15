3d52126adb6a047350d2b85b6819d89f28356600
JocaMartins33 <jokaslab233@gmail.com>
2024-07-15 22:10:49 +0100

    Practice-Python

---
+++ course/10.while_loops.py
@@ -0,0 +1,11 @@
+# while loops
+
+# while 1==1:
+    # print("Help I'm stuck in a loop!")
+    
+name = ""
+
+while len(name) == 0:
+    name = input("Enter your name: ")
+    
+print("Hello " + name)
