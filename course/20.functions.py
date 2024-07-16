86106cd153f29a72234912dd067199fc1d4b1d43
JocaMartins33 <jokaslab233@gmail.com>
2024-07-16 10:09:59 +0100

    Practice-Python

---
+++ course/20.functions.py
@@ -0,0 +1,11 @@
+# functions
+
+def hello(name1, name2, age):
+    print("Hello! " + name1 + " you have " + str(age) + " yr.")
+    print("Hello! " + name2)
+   
+my_name = "Joca"
+age = 20
+other_name = "jovem" 
+hello(my_name, other_name, age)
+
