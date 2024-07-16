35ef4b6422f2851f7f0fbaea875046e1a624cf11
JocaMartins33 <jokaslab233@gmail.com>
2024-07-16 09:52:36 +0100

    Practice-Python

---
+++ course/19.indexing.py
@@ -0,0 +1,16 @@
+# indexing
+
+name = "joca MARTINS!"
+
+if(name[0].islower):
+    name = name.capitalize()
+print(name)
+   
+first_name = name[:4].upper()
+print(first_name)
+
+last_name = name[-8:-1].lower()
+print(last_name)
+
+last_character = name[-1]
+print(last_character)
