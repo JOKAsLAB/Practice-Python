f27dcb5edf111d737f9d7f62a2cf04626681697a
JocaMartins33 <jokaslab233@gmail.com>
2024-07-18 15:07:50 +0100

    Practice-Python

---
+++ course/29.exception_handling.py
@@ -0,0 +1,18 @@
+# exception handling
+try:
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+except ZeroDivisionError as e:
+    print(e)
+    print("You can't divide by zero!")
+except ValueError as e:
+    print(e)
+    print("Enter only numbers pls")
+except Exception as e:
+    print(e)
+    print("something went wrong :/")
+else:
+    print(result)
+finally:
+    print("This will always execute")
