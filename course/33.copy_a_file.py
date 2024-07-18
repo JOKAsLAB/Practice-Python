e5fef047d5761c22d81541d279e856cbf91d2dd9
JocaMartins33 <jokaslab233@gmail.com>
2024-07-18 16:06:14 +0100

    Practice-Python

---
+++ course/33.copy_a_file.py
@@ -0,0 +1,7 @@
+# copy a file
+
+import shutil
+
+shutil.copyfile("D:\\git hub\\Practice-Python\\course\\33.test.txt", "course\\33.copy.txt")
+# shutil.copy("D:\\git hub\\Practice-Python\\course\\33.test.txt", "course\\33.copy.txt")
+# shutil.copy2("D:\\git hub\\Practice-Python\\course\\33.test.txt", "course\\33.copy.txt")
