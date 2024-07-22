df3810a717853a4bd58a0cec4409ca2b761ec30d
JocaMartins33 <jokaslab233@gmail.com>
2024-07-22 17:52:30 +0100

    Practice-Python

---
+++ course/49.duck_typing.py
@@ -0,0 +1,27 @@
+# duck typing
+
+class Duck:
+    def walk(self):
+        print("This duck is walking")
+        
+    def talk(self):
+        print("This duck is qwaucking")
+        
+class Chicken:
+    def walk(self):
+        print("This chicken is walking")
+        
+    def talk(self):
+        print("This chicken is clucking")
+        
+class Person():
+    def catch(sef, duck):
+        duck.walk()
+        duck.talk()
+        print("You caught the critter")
+        
+duck = Duck()
+chicken = Chicken()
+person = Person()
+
+person.catch(chicken)
