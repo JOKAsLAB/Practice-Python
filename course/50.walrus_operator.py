197b137bf438217d67f386ac134a6682e7c7419f
JocaMartins33 <jokaslab233@gmail.com>
2024-07-23 14:19:11 +0100

    Practice-Python

---
+++ course/50.walrus_operator.py
@@ -0,0 +1,20 @@
+# walrus operator
+
+# happy = True
+# print(happy)
+
+print(happy := True)
+
+print("--------------------")
+
+# foods = list()
+# while True:
+    # food = input("What food do you like?: ")
+    # if food == "quit":
+        # break
+    # foods.append(food)
+    
+foods = list()
+while food:= input("What food do you like?: ") != "quit":
+    foods.append(food)
+    
