bc764b1c272aa3a29475666431debae1472b5757
JocaMartins33 <jokaslab233@gmail.com>
2024-07-25 17:18:36 +0100

    Practice-Python

---
+++ course/64.daemon_threads.py
@@ -0,0 +1,23 @@
+# daemon threads
+
+import threading
+import time
+
+def timer():
+    print()
+    print()
+    count = 0
+    while True:
+        time.sleep(1)
+        count += 1
+        print("logged in for: ", count, "seconds")
+
+
+
+x = threading.Thread(target=timer, daemon=True)
+x.start()
+
+# x.setDaemon(True)
+# print(x.isDaemon())
+
+answer = input("Do you want to exit?")
