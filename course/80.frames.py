06de7ec7c7ec973b1dee3fdd4c1d0ea3d22132c1
JocaMartins33 <jokaslab233@gmail.com>
2024-07-30 15:16:35 +0100

    Practice-Python

---
+++ course/80.frames.py
@@ -0,0 +1,15 @@
+# frames
+
+from tkinter import *
+
+window = Tk()
+
+frame = Frame(window, bg="pink", bd=5, relief=SUNKEN)
+frame.place(x=0,y=0)
+
+button = Button(frame, text="W", font=("Consolas, 25"), width=3).pack(side=TOP)
+button = Button(frame, text="A", font=("Consolas, 25"), width=3).pack(side=LEFT)
+button = Button(frame, text="S", font=("Consolas, 25"), width=3).pack(side=LEFT)
+button = Button(frame, text="D", font=("Consolas, 25"), width=3).pack(side=LEFT)
+
+window.mainloop()
