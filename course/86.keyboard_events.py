a20adf99d09ca82b39ed954d9a7338eb0d085818
JocaMartins33 <jokaslab233@gmail.com>
2024-08-01 10:57:35 +0100

    Practice-Python

---
+++ course/86.keyboard_events.py
@@ -0,0 +1,16 @@
+# keyboard events
+
+from tkinter import *
+
+def doSomething(event):
+    # print("You did a thing" + event.keysym)
+    label.config(text= event.keysym)
+
+window = Tk()
+
+window.bind("<Key>", doSomething)
+
+label = Label(window, font=("Helvetica",100))
+label.pack()
+
+window.mainloop()
