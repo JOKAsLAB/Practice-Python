5b6f87ac64f25abe661674c07ef2d9440c3c139e
JocaMartins33 <jokaslab233@gmail.com>
2024-08-02 15:57:05 +0100

    Practice-Python

--- course/91.multiple_animations.py
+++ course/91.multiple_animations.py
@@ -1,7 +1,7 @@
 # multiple animations
 
 from tkinter import *
-from Ball import *
+# from Ball import *
 import time
 
 window = Tk()
@@ -12,14 +12,14 @@ HEIGHT = 500
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-volley_ball = Ball(canvas, 0, 0, 100, 1, 1, "white")
-tennis_ball = Ball(canvas, 0, 0, 50, 4, 3, "yellow")
-basket_ball = Ball(canvas, 0, 0, 125, 8, 7, "orange")
+# volley_ball = Ball(canvas, 0, 0, 100, 1, 1, "white")
+# tennis_ball = Ball(canvas, 0, 0, 50, 4, 3, "yellow")
+# basket_ball = Ball(canvas, 0, 0, 125, 8, 7, "orange")
 
 while True:
-    volley_ball.move()
-    tennis_ball.move()
-    basket_ball.move()
+    # volley_ball.move()
+    # tennis_ball.move()
+    # basket_ball.move()
     window.update()
     time.sleep(0.01)
 
